a4c2a56b1096ffb58db7ea1f658e97f9a97111b8
VinithReddy21 <saivinny@gmail.com>
2025-04-04 15:45:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-import praw
 import pandas as pd
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
-from datetime import datetime
-from transformers import pipeline
 
 nltk.download('vader_lexicon')
 
